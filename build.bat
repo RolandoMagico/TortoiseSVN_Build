f88405113c16ae548fef82b6bb399864538d6c8a
ContextQuickie <ContextQuickie@users.noreply.github.com>
2018-12-16 10:38:59 +0100

    Update for building the svnjavahl project in the Tortoise SVN working copy

--- build.bat
+++ build.bat
@@ -1,10 +1,13 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Configuration
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Tortoise SVN version (must equal a tag name from http://svn.osdn.net/svnroot/tortoisesvn/tags/)
+SET TortoiseSvnVersion=version-1.10.1
+
 :: NAnt version
 SET NANT_VERSION=0.92
 :: Path to NAnt
-SET PATH=%PATH%;%~dp0\nant-0.92\bin
+SET PATH=%PATH%;%~dp0\%TortoiseSvnVersion%\nant-0.92\bin
 :: Path to python 2.6 (x86)
 SET PATH=%PATH%;C:\Python26
 :: Path to wget
@@ -15,19 +18,19 @@ SET SEVEN_ZIP="C:\Program Files\7-Zip\7z.exe"
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Preparation
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-IF NOT EXIST nant-%NANT_VERSION%\ (
-wget http://netcologne.dl.sourceforge.net/project/nant/nant/%NANT_VERSION%/nant-%NANT_VERSION%-bin.zip
-%SEVEN_ZIP% x nant-%NANT_VERSION%-bin.zip
+IF NOT EXIST %TortoiseSvnVersion%\nant-%NANT_VERSION%\ (
+wget http://netcologne.dl.sourceforge.net/project/nant/nant/%NANT_VERSION%/nant-%NANT_VERSION%-bin.zip --output-document=%TortoiseSvnVersion%\nant-%NANT_VERSION%-bin.zip
+%SEVEN_ZIP% x %TortoiseSvnVersion%\nant-%NANT_VERSION%-bin.zip -o%TortoiseSvnVersion%
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Execution
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" 8.1
-NAnt.exe -buildfile:%~dp0\build.xml -logfile:build32.log -D:arch=x86 || GOTO END
+NAnt.exe -buildfile:%~dp0\build.xml -logfile:build32.log -D:arch=x86 -D:TortoiseSvnVersion=%TortoiseSvnVersion% || GOTO END
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" 8.1
-NAnt.exe -buildfile:%~dp0\build.xml -logfile:build64.log -D:arch=x64 || GOTO END
+NAnt.exe -buildfile:%~dp0\build.xml -logfile:build64.log -D:arch=x64 -D:TortoiseSvnVersion=%TortoiseSvnVersion% || GOTO END
 
 :END
 pause
