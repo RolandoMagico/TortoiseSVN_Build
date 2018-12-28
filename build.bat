19dccb918225dc3e55c9685b37a294c6cdf63035
ContextQuickie <ContextQuickie@users.noreply.github.com>
2018-12-28 10:51:48 +0100

    Updated for Tortoise SVN 1.8.12 build with Visual Studio 2012 Update 3

--- build.bat
+++ build.bat
@@ -35,10 +35,10 @@ wget http://netcologne.dl.sourceforge.net/project/nant/nant/%NANT_VERSION%/nant-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Execution
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat" 8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\vcvars32.bat" 8.1
 NAnt.exe -buildfile:%~dp0\build.xml -logfile:build32.log -D:arch=x86 -D:TortoiseSvnVersion=%TortoiseSvnVersion% -D:JdkDirectory=%JDK_PATH% || GOTO END
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" 8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\amd64\vcvars64.bat" 8.1
 NAnt.exe -buildfile:%~dp0\build.xml -logfile:build64.log -D:arch=x64 -D:TortoiseSvnVersion=%TortoiseSvnVersion% -D:JdkDirectory=%JDK_PATH% || GOTO END
 
 :END
